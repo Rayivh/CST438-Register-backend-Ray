dc8daf46b149793e7a5dc59882b767e574d2be54
RayVanHorn <61771343+RayVanHorn@users.noreply.github.com>
2023-02-28 23:35:05 -0800

    Played around with making StudentController & StudentRepository.

--- cst438_register/src/main/java/com/cst438/controller/StudentController.java
+++ cst438_register/src/main/java/com/cst438/controller/StudentController.java
@@ -1,13 +1,79 @@
 package com.cst438.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.cst438.domain.Course;
+import com.cst438.domain.CourseRepository;
+import com.cst438.domain.Enrollment;
+import com.cst438.domain.EnrollmentRepository;
+import com.cst438.domain.ScheduleDTO;
+import com.cst438.domain.Student;
+import com.cst438.domain.StudentRepository;
+import com.cst438.service.GradebookService;
 
 @RestController
 public class StudentController {
 
-	@PostMapping("/student")
-	public String createNewStudent() {
-		return "student id = 12398";
+	
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
+	
+	@GetMapping("/student")
+	public Student getStudentbyEmail( @RequestBody String email) {
+		Student student = studentRepository.findByEmail(email);
+		if (student != null) {
+			System.out.println("/student student "+student.getName()+" "+student.getStudent_id());
+			return student;
+		}else {
+			throw  new ResponseStatusException(HttpStatus.BAD_REQUEST, "Student not found.  ");
+		}
+		
 	}
+	
+//	@PostMapping("/student")
+//	@Transactional
+//	public String createNewStudent() {
+//		return "student id = 12398";
+//	}
+	
+
+	@PostMapping("/student")
+	@Transactional
+	public Student createNewStudent( @RequestBody String email, @RequestBody String name) {
+		
+		//Check for existing student
+		//List<Student> studentList = studentRepository.findStudentByEmail(email);
+		//Student student = studentList.get(0);
+		
+		Student student = studentRepository.findByEmail(email);
+		
+		if (student == null) {
+			//Make new student
+			student = new Student();
+			student.setEmail(email);
+			student.setName(name);
+			
+			Student savedStudent = studentRepository.save(student);
+			//ScheduleDTO.CourseDTO result = createCourseDTO(savedStudent);  do I need a StudentDTO (and setter function below?)
+			return savedStudent;
+		}else {
+			throw  new ResponseStatusException( HttpStatus.BAD_REQUEST, "Student already exists in database.  "+student.getStudent_id());
+		}
+		
+
+	}
+	
+
 }
--- cst438_register/src/main/java/com/cst438/domain/StudentRepository.java
+++ cst438_register/src/main/java/com/cst438/domain/StudentRepository.java
@@ -1,11 +1,32 @@
 package com.cst438.domain;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface StudentRepository extends CrudRepository <Student, Integer> {
 	
 	// declare the following method to return a single Student object
 	// default JPA behavior that findBy methods return List<Student> except for findById.
+	
 	public Student findByEmail(String email);
-
+	
+//	@Query("select e from Student e where e.student.email=:email")
+//	List<Student> findByEmail(String email);
+//
+//	@Query("select 1 from Student e where e.student.email=:email and e.name=:name")
+//	public List<Student> findStudentByEmail(@Param("email") String email);
+//	
+//	@Query("select 1 from Student e where e.student.email=:email")
+//	public Student findStudentByEmail(@Param("email") String email);
+//	
+//	@Query("select e from Student e where e.student.email=:email")
+//	public Student findByEmail(@Param("email") String email);
+//
+//	@SuppressWarnings("unchecked")
+//	Student save(Student e);
+//
 }
+	
